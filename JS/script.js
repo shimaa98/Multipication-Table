0c5a9ad056576e832ecf3c78659ba420847d5cdc
shimaa191 <shimaafathi982@gmail.com>
2023-09-07 05:00:39 +0300

    first commit

---
+++ JS/script.js
@@ -0,0 +1,21 @@
+const table = document.getElementById("mul_table"),
+  form = document.getElementById("form");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  let header = `<tr>
+  <th>Multiplicand</th>
+  <th>Multiplier</th>
+  <th>Product</th>
+    </tr>`;
+  table.innerHTML = header;
+  const number = +form.elements.number.value.trim();
+  for (let i = 0; i < 10; i++) {
+    let ele = `<tr>
+    <td>${i + 1}</td>
+    <td>${number}</td>
+    <td>${(i + 1) * number}</td>
+    </tr>`;
+    table.innerHTML += ele;
+  }
+});
